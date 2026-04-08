510d47b3ecd67139d0221994bd97ad3b905dc8da
Lev <lyovademyanov@gmail.com>
2026-04-08 14:02:51 +0300

    мяу

--- src/main/java/by/lyofchik/quiz/Service/QuizzesService.java
+++ src/main/java/by/lyofchik/quiz/Service/QuizzesService.java
@@ -3,16 +3,22 @@ package by.lyofchik.quiz.Service;
 import by.lyofchik.quiz.Model.DTO.Request.QuizRq;
 import by.lyofchik.quiz.Model.DTO.Request.QuizzesRq;
 import by.lyofchik.quiz.Model.DTO.Response.Response;
+import by.lyofchik.quiz.Model.Entity.Question;
+import by.lyofchik.quiz.Model.Entity.Quiz;
+import by.lyofchik.quiz.Model.Mapper.QuizMapper;
 import by.lyofchik.quiz.Repository.QuizzesRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class QuizzesService {
     QuizzesRepository quizzesRepository;
+    QuizMapper quizMapper;
 
     public Response allQuizzes(QuizzesRq request) {
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
@@ -25,7 +31,23 @@ public class QuizzesService {
 
 
     public Response createQuiz(QuizRq request) {
-
+        try {
+            Quiz newQuiz = quizMapper.toQuiz(request);
+            request.getQuestions()
+                    .forEach(q -> {
+                        Question question = quizMapper.toQuestion(q, newQuiz.getId());
+                            newQuiz.getQuestions().add(question);
+                            q.getAnswers()
+                            .forEach(a -> {
+                                    question.getAnswers().add(quizMapper.toAnswer(a, newQuiz.getId()));
+                    });
+        });
+            quizzesRepository.save(newQuiz);
+        }  catch (Exception e) {
+            log.error(e.getMessage());
+            Response.error();
+        }
+        return Response.success();
     }
 
     public Response deleteQuiz(int id) {
@@ -34,5 +56,6 @@ public class QuizzesService {
     }
 
     public Response updateQuiz(int id, QuizRq request) {
+
     }
 }
